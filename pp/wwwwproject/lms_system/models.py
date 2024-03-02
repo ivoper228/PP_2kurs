8922463a6b5cca9db33df33a47268edc5bc4141b
ivoper228 <papa_niga_mama@mail.ru>
2024-03-02 13:59:59 +0500

    Перенес архитектуру БД

--- pp/wwwwproject/lms_system/models.py
+++ pp/wwwwproject/lms_system/models.py
@@ -2,38 +2,60 @@ from django.db import models
 
 # Create your models here.
 """
-CREATE TABLE [rules]( --??????? ?????
-	id INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE lessons( --??????? ??????
+	id INT PRIMARY KEY  IDENTITY(1,1), 
+	id_topic INT CONSTRAINT FK_TOPIC_LESSONS REFERENCES topic(id),
 	[name] NVARCHAR(40) NOT NULL,
-	[level] INT NOT NULL
+	answer TEXT NOT NULL,
+	istrue BIT NOT NULL
 )"""
 class Rules(models.Model):
-	pass
+	name = models.CharField(max_length=128)
+	level = models.IntegerField
 
 class Topic(models.Model):
-	pass
+	name = models.CharField(max_length=128)
+	is_public = models.BinaryField
+	id_rule=models.ForeignKey(Rules)
 
 class Worker(models.Model):
-	pass
+	name = models.CharField(max_length=128)
+	fname = models.CharField(max_length=128)
+	id_parent = models.ForeignKey(self)
+	id_rule = models.ForeignKey(Rules)
 
 class Files(models.Model):
-	pass
+	id_worker = models.ForeignKey(Worker)
+	image = models.ImageField
 
 class Tests(models.Model):
-	pass
+	id_topic = models.ForeignKey(Topic)
+	questions = models.TextField
 
 class Answers(models.Model):
-	pass
-class Lessond(models.Model):
-	pass
+	id_test = models.ForeignKey(Tests)
+	answer = models.TextField
+	istrue = models.BinaryField
+class Lesson(models.Model):
+	id_topic = models.ForeignKey(Topic)
+	name = models.CharField(max_length=128)
+	answer = models.TextField
+	istrue = models.BinaryField
 
 class Subjects(models.Model):
-	pass
+	id_lesson = models.ForeignKey(Lesson)
+	name = models.CharField(max_length=128)
+	answer = models.TextField
+	image = models.ImageField
 
 class Progresstests(models.Model):
-	pass
+	id_worker = models.ForeignKey(Worker)
+	id_test = models.ForeignKey(Tests)
+	status = models.BooleanField(null=True)
 
 class Progresslesons(models.Model):
-	pass
+	id_worker = models.ForeignKey(Worker)
+	id_test = models.ForeignKey(Lesson)
+	status = models.BooleanField(null=True)
 
 
