c56188461b456ff1517215f647df9652ace9312d
ivoper228 <papa_niga_mama@mail.ru>
2024-03-03 14:35:18 +0500

    Фронтенд(Сайт с мужиками и женщинами)

--- pp/wwwwproject/lms_system/urls.py
+++ pp/wwwwproject/lms_system/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('invate/<urlgen>', views.invate),
+    path('invate/lk/<urlgen>', views.invate, name='lk'),
+    path('invate/<urlgen>', views.lk, name='topic'),
 ]
--- pp/wwwwproject/lms_system/views.py
+++ pp/wwwwproject/lms_system/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Invite
+from .models import *
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
 
 def invate(request, urlgen):
 
     try:
-        m = Invite.objects.get(urlgen = urlgen)  # ObjectDoesNotExist
+      m: Invite = Invite.objects.get(urlgen = urlgen)  # ObjectDoesNotExist
     except ObjectDoesNotExist:
         return render(request, "lms_system/404.html")
-    return render(request, "lms_system/file.html")
+
+    if Worker.objects.filter(id = m.id_worker):
+        return render(request, "lms_system/file.html", {'urlgen':urlgen})#заходит на страницу курса
+
+    #try:
+    #   with transaction.atomic():
+    n: Worker = Worker()
+    n.name = " "
+    n.fname = " "
+    n.mail = m.mail
+    n.id_parent = m.id_worker_parents
+    n.id_rule = Rules.objects.get(id=1)
+    n.save()
+    m.id_worker = n.pk
+    m.save()
+    #except IntegrityError:
+    #    return render(request, "lms_system/404.html")
+
+
+    return lk(request, urlgen)
+
+def lk(request, urlgen):
+    return render(request, "lms_system/lk.html",{'urlgen':urlgen})
 
